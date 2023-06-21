0937b708fa9703400875921eadc392fc90759a0d
niyoyigenera <niyooiane@gmail.com>
2023-06-21 10:32:46 +0200

    initialize project

---
+++ javascript/students.js
@@ -0,0 +1,47 @@
+// window.document.getElementById("heading2").style.display="none"
+// window.document.getElementById("hod1").style.color="red"
+// window.document.getElementById("hod1").style.fontWeight="900"
+
+// let firstName="niwemwiza";
+// let number1 =900;
+// let number2 =2000
+// let sum =number1+number2;
+// console.log(firstName);
+// console.log("the first number is",number1,"second number is",number2,"the sum of those 2 number are ",sum)
+
+// naming conversion
+// if statment
+// loop statement
+// dom manupulation
+// event
+
+// naming conversion
+  // let firstName
+  // let first-name
+
+//   if statement
+let ages =20;
+// how if statement works
+if(ages<20){
+console.log("you are steal young")
+
+}
+// how if-else statement works
+if(ages<20){
+    console.log("you are steal young")
+}
+else{
+    console.log("you are adult!!!!")
+}
+
+// how nested if statement works
+if (ages <20){
+    console.log("you are steal young")
+}
+else if(ages==20){
+    console.log("welcome to the show")
+}
+else{
+    console.log("your are adult")
+}
+
